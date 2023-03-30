844bcfca17c070b9ac1152084e79dfc838f503e4
mahdiharish <mahdiharish12@gmail.com>
2023-03-30 20:37:34 +0800

    add feature for register new user

--- index.js
+++ index.js
@@ -27,35 +27,64 @@ document
   .getElementById("registerPassword")
   .addEventListener("input", checkRegisterPassword);
 
+// API ENDPOINT FOR USER
+
+const API_USER_ENDPOINT = "https://642433294740174043359209.mockapi.io/user";
+
+// REGISTER NEW USER TO API USER ENDPOINT
+let registerNewUser = async () => {
+  try {
+    const response = await fetch(API_USER_ENDPOINT, {
+      method: "POST",
+      body: JSON.stringify({
+        email: `${document.getElementById("registerEmail").value}`,
+        password: `${document.getElementById("registerPassword").value}`,
+        id: ``,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const user = await response.json();
+    console.log(user);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 // REGISTER NEW USER
-const userList = JSON.parse(localStorage.getItem("users")) || [
-  { email: "admin@gmail.com", password: "Admin123!" },
-];
-document.getElementById("registerBtn").addEventListener("click", function (e) {
-  e.preventDefault();
-  const email = document.getElementById("registerEmail");
-  const password = document.getElementById("registerPassword");
-  let validEmail = true;
-  let validPassword = true;
-  checkRegisterEmail();
-  checkRegisterPassword();
-  if (document.getElementById("emailValidation").innerHTML !== "") {
-    validEmail = false;
-  }
-  if (document.getElementById("pwValidation").innerHTML !== "") {
-    validPassword = false;
-  }
-  if (validEmail && validPassword) {
-    const isEmailRegistered = userList.some(
-      (user) => user.email === email.value
-    );
-    if (isEmailRegistered) {
+const emailInput = document.getElementById("registerEmail");
+const passwordInput = document.getElementById("registerPassword");
+document
+  .getElementById("registerBtn")
+  .addEventListener("click", async function (e) {
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    try {
+      const response = await fetch(API_USER_ENDPOINT);
+      const users = await response.json();
+      const isEmailRegistered = users.some((user) => user.email === email);
+      if (isEmailRegistered) {
+        document.getElementById("emailValidation").innerHTML =
+          "Email is already registered!";
+        return;
+      }
+      const newUser = { email, password };
+      const createUserResponse = await fetch(API_USER_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+      if (!createUserResponse.ok) {
+        throw new Error("Failed to create user");
+      }
+      window.location.href = "./login.html";
+    } catch (error) {
+      console.error(error);
       document.getElementById("emailValidation").innerHTML =
-        "Email is already registered!";
-      return;
+        "Failed to register user. Please try again later.";
     }
-    userList.push({ email: email.value, password: password.value });
-    localStorage.setItem("users", JSON.stringify(userList));
-    window.location.href = "./signin/signin.html";
-  }
-});
+  });
